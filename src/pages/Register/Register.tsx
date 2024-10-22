c948d6f9dd1dbdeaafc9a36f8b1a510c790930df
Huu Hung <hungmnh01@gmail.com>
2024-10-22 09:10:43 +0700

    done handle 422 error

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,17 +1,56 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { useMutation } from '@tanstack/react-query'
+import { omit } from 'lodash'
 import Input from 'src/components/input'
 import { getRules, schema, Schema } from 'src/utils/rules'
+import { registerAccount } from 'src/apis/auth.apis'
+import { isAxiosUnprocessableEntity } from 'src/utils/utils'
+import { ResponseApi } from 'src/types/utils.type'
 
 type FormData = Schema
 
 
 export default function Register() {
-  const { register, handleSubmit, formState: { errors }, getValues } = useForm<FormData>({ resolver: yupResolver(schema) })
+  const { register, handleSubmit, formState: { errors }, setError } = useForm<FormData>({ resolver: yupResolver(schema) })
+
+  const registerAccountMutation = useMutation({
+    mutationFn: (body: Omit<FormData, 'confirm_password'>) => registerAccount(body)
+  })
 
   const onSubmit = handleSubmit((data) => {
-    console.log(data)
+    const body = omit(data, ['confirm_password'])
+    registerAccountMutation.mutate(body, {
+      onSuccess: (data) => {
+        console.log(data)
+      },
+      onError: (error) => {
+        if (isAxiosUnprocessableEntity<ResponseApi<Omit<FormData, 'confirm_password'>>>(error)) {
+          const formError = error.response?.data.data
+          if (formError) {
+            Object.keys(formError).forEach((key) => {
+              setError(key as keyof Omit<FormData, 'confirm_password'>, {
+                message: formError[key as keyof Omit<FormData, 'confirm_password'>],
+                type: 'Sever'
+              })
+            })
+          }
+          // if (formError?.email) {
+          //   setError('email', {
+          //     message: formError.email,
+          //     type: 'Sever'
+          //   })
+          // }
+          // if (formError?.password) {
+          //   setError('password', {
+          //     message: formError.password,
+          //     type: 'Sever'
+          //   })
+          // }
+        }
+      }
+    })
   })
   return (
     <div className='bg-shopee_orange'>
