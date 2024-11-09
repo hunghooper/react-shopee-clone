acfb9bbbfb6b10d70982a41bb04e42c13b0bf053
Huu Hung <hungmnh01@gmail.com>
2024-11-09 16:05:39 +0700

    done AsideFilter-UI

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,17 +1,17 @@
+import { InputHTMLAttributes } from 'react'
 import { RegisterOptions, UseFormRegister } from 'react-hook-form'
 
-type Props = {
-  className?: string
-  type: React.HTMLInputTypeAttribute
-  name: string
-  placeholder?: string
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  register: UseFormRegister<any>
+  register?: UseFormRegister<any>
   rules?: RegisterOptions
-  autoComplete: string
+  classNameInput?: string
+  classNameError?: string
 }
 export default function Input({
   className,
+  classNameError = 'mt-1 text-red-600 min-h-[1rem] text-sm',
+  classNameInput = 'p-3 w-full outline-none border border-gray-200 focus:border-gray-300 rounded-sm focus:shadow-sm',
   name,
   placeholder,
   type,
@@ -20,16 +20,17 @@ export default function Input({
   rules,
   autoComplete
 }: Props) {
+  const registerResult = register && name ? register(name, rules) : {}
   return (
     <div className={className}>
       <input
         type={type}
         placeholder={placeholder}
         autoComplete={autoComplete}
-        {...register(name, rules)}
-        className='p-3 w-full outline-none border border-gray-200 focus:border-gray-300 rounded-sm focus:shadow-sm'
+        {...registerResult}
+        className={classNameInput}
       />
-      <div className='mt-1 text-red-600 min-h-[1rem] text-sm'>{errorMessage}</div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
---
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -0,0 +1,10 @@
+
+export default function SortProductList() {
+  return (
+    <div className="bg-gray-300/40 py-4 px-3">
+      <div className="flex flex-wrap items-center gap-2">
+        <div>Sort by</div>
+      </div>
+    </div>
+  )
+}
